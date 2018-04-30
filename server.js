f6e8b7cade3f94bdefb4a830860df2a74024b6c5
Collin Hintzke <collinhintzke5@gmail.com>
2018-04-30 14:13:52 -0700

    set up DB

--- server.js
+++ server.js
@@ -19,3 +19,48 @@ client.connect();
 client.on('error', err => console.error(err));
 
 app.get('/test', (req, res) => res.send('hello world'));
+app.get('/', (req, res) => res.redirect(CLIENT_URL));
+
+
+// app.get('/api/v1/books', (req, res) => {
+//   client.query('SELECT * from books;')
+//     .then(results => res.send(results.row))
+//     .catch(console.error);
+
+// });
+
+
+app.get('*', (req, res) => res.sendStatus('you didnt get there'));
+
+
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
+
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    users (
+      user_id SERIAL PRIMARY KEY,
+      user_name VARCHAR(255) UNIQUE NOT NULL,
+      email VARCHAR (255) UNIQUE NOT NULL,
+      public BOOLEAN,
+    );`
+  )
+    // .then(loadUsers)
+    .catch(console.error);
+
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    trips (
+      trips_id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(user_id),
+      country VARCHAR(100) NOT NULL,
+      city VARCHAR(100),
+      start_date DATE,
+      end_date DATE,
+    );`
+  )
+    // .then(loadtrips)
+    .catch(console.error);
+}
