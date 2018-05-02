ccf89888ff073c78a855a98a54f1f80fc42cd39b
Mason Bassett <bassettmason@gmail.com>
2018-05-01 17:28:38 -0700

    server

--- server.js
+++ server.js
@@ -29,19 +29,41 @@ app.get('/test', (req, res) => res.send('hello world'));
 
 
 app.get('/trips', (req, res) => {
-  console.log('in trips')
+  
   client.query('SELECT * FROM trips;')
     .then(results =>{
-console.log('results: ',results)
-     res.send(results.row)
+
+     res.send(results.rows)
     })
-    .then(console.log('retriev'))
+    
     .catch(console.error);
 
 });
 
 
 
+// app.post('/login', (req, res) => {
+//   let {user_name, email, public, password} = req.body;
+//   client.query(`
+//     INSERT INTO users(user_name, email, public, password) VALUES($1, $2, $3, $4)`,
+//     [user_name, email, public, password]
+//   )
+//   .then(results => res.send('new data user'))
+//   .catch(console.error);
+// });
+
+
+
+//masons .post attempt commented because no faith
+app.post('/addtrip', (req, res) => {
+  let {user_id, country, city, start_date, end_date} = req.body;
+  client.query(`
+    INSERT INTO users(user_id, country, city, start_date, end_date) VALUES($1, $2, $3, $4)`,
+    [user_id, country, city, start_date, end_date]
+  )
+  .then(results => res.send('new data user'))
+  .catch(console.error);
+});
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
@@ -67,8 +89,7 @@ function loadDB() {
     CREATE TABLE IF NOT EXISTS
     trips (
       trips_id SERIAL PRIMARY KEY,
-      user_id INTEGER,
-      FOREIGN KEY (user_id) REFERENCES users(user_id),
+      user_id INTEGER REFERENCES users(user_id),
       country VARCHAR(100) NOT NULL,
       city VARCHAR(100),
       start_date DATE,
