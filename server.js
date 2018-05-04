438a77b6a8490a4c69d7a243a7d7305b89c8a5f1
Andrew Baik <andrewbaik@outlook.com>
2018-05-04 12:54:57 -0700

    fixed constring

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:8080';
 
 // process.env.DATABASE_URL = 'postgres://postgres:1234@localhost:5432/travelapp';
-process.env.DATABASE_URL = 'postgres://localhost:5432';
+// process.env.DATABASE_URL = 'postgres://localhost:5432';
 
 
 // Database Setup
